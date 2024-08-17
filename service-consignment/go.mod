b57691a28a9f8d1bf70021291f2d3f98c71e2995
s.masoumbeigi <sbeygi712@gmail.com>
2024-08-17 16:47:38 +0330

    vessel service implemented and dockerized

--- service-consignment/go.mod
+++ service-consignment/go.mod
@@ -1,12 +1,31 @@
-module github.com/SoroushBeigi/microservice-go
+module github.com/SoroushBeigi/microservice-go/service-consignment
 
 go 1.22.3
 
 require (
+	google.golang.org/grpc v1.65.0
+	google.golang.org/protobuf v1.34.2
+)
+
+require (
+	github.com/bitly/go-simplejson v0.5.0 // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/fsnotify/fsnotify v1.6.0 // indirect
+	github.com/go-micro/generator v0.0.0-20221011190410-18c90eb78ace // indirect
+	github.com/golang/protobuf v1.5.4 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/imdario/mergo v0.3.12 // indirect
+	github.com/miekg/dns v1.1.43 // indirect
+	github.com/oxtoacart/bpool v0.0.0-20190530202638-03653db5a59c // indirect
+	github.com/patrickmn/go-cache v2.1.0+incompatible // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/urfave/cli/v2 v2.25.7 // indirect
+	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
+	go-micro.dev/v5 v5.3.0 // indirect
 	golang.org/x/net v0.28.0 // indirect
+	golang.org/x/sync v0.8.0 // indirect
 	golang.org/x/sys v0.24.0 // indirect
 	golang.org/x/text v0.17.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240814211410-ddb44dafa142 // indirect
-	google.golang.org/grpc v1.65.0 // indirect
-	google.golang.org/protobuf v1.34.2 // indirect
 )
--- service-consignment/main.go
+++ service-consignment/main.go
@@ -3,64 +3,61 @@ package main
 import (
 	"context"
 	pb "github.com/SoroushBeigi/microservice-go/service-consignment/proto/consignment"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/reflection"
+	"go-micro.dev/v5"
 	"log"
-	"net"
-	"sync"
-)
-
-const (
-	port = ":50051"
 )
 
 type repository interface {
 	Create(*pb.Consignment) (*pb.Consignment, error)
+	GetAll() []*pb.Consignment
 }
 
 type Repository struct {
-	mu           sync.RWMutex
 	consignments []*pb.Consignment
 }
 
 func (repo *Repository) Create(consignment *pb.Consignment) (*pb.Consignment, error) {
-	repo.mu.Lock()
 	updated := append(repo.consignments, consignment)
 	repo.consignments = updated
-	repo.mu.Unlock()
 	return consignment, nil
 }
 
-type service struct {
-	pb.UnimplementedShippingServiceServer
+func (repo *Repository) GetAll() []*pb.Consignment {
+	return repo.consignments
+}
+
+type consignmentService struct {
 	repo repository
 }
 
-func (s *service) mustEmbedUnimplementedShippingServiceServer() {}
-
-func (s *service) CreateConsignment(context context.Context, req *pb.Consignment) (*pb.Response, error) {
+func (s *consignmentService) CreateConsignment(context context.Context, req *pb.Consignment, res *pb.Response) error {
 	consignment, err := s.repo.Create(req)
 	if err != nil {
-		return nil, err
+		return err
 	}
-	return &pb.Response{Created: true, Consignment: consignment}, nil
+	res.Created = true
+	res.Consignment = consignment
+	return nil
+}
+
+func (s *consignmentService) GetConsignments(context context.Context, req *pb.GetRequest, res *pb.Response) error {
+	consignments := s.repo.GetAll()
+	res.Consignments = consignments
+	return nil
 }
 
 func main() {
 	repo := &Repository{}
 
-	lis, err := net.Listen("tcp", port)
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+	service := micro.NewService(micro.Name("service.consignment"))
+
+	service.Init()
+
+	if err := pb.RegisterShippingServiceHandler(service.Server(), &consignmentService{repo: repo}); err != nil {
+		log.Panic(err)
 	}
-	server := grpc.NewServer()
 
-	pb.RegisterShippingServiceServer(server, &service{repo: repo})
-
-	reflection.Register(server)
-
-	log.Println("Running on port: ", port)
-	if err := server.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
+	if err := service.Run(); err != nil {
+		log.Panic(err)
 	}
 }
