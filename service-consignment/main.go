1e0794bcaec2f2b3bf7d2c6543f2db5fb5b18463
s.masoumbeigi <sbeygi712@gmail.com>
2024-08-19 11:30:21 +0330

    consignment service refactor

--- service-consignment/main.go
+++ service-consignment/main.go
@@ -1,91 +1,58 @@
 package main
 
 import (
-	pb "github.com/SoroushBeigi/microservice-go/service-consignment/proto/consignment"
-	vesselProto "github.com/SoroushBeigi/microservice-go/service-vessel/proto/vessel"
+	"context"
 	"go-micro.dev/v4"
 	"go-micro.dev/v4/logger"
-	"golang.org/x/net/context"
+	"go.mongodb.org/mongo-driver/mongo"
 	"log"
+	"os"
+
+	pb "github.com/SoroushBeigi/microservice-go/service-consignment/proto/consignment"
+	vesselProto "github.com/SoroushBeigi/microservice-go/service-vessel/proto/vessel"
 )
 
-type Repository interface {
-	Create(*pb.Consignment) (*pb.Consignment, error)
-	GetAll() []*pb.Consignment
-}
-
-type ConsignmentRepository struct {
-	consignments []*pb.Consignment
-}
-
-func (repo *ConsignmentRepository) Create(consignment *pb.Consignment) (*pb.Consignment, error) {
-	updated := append(repo.consignments, consignment)
-	repo.consignments = updated
-	return consignment, nil
-}
-
-func (repo *ConsignmentRepository) GetAll() []*pb.Consignment {
-	return repo.consignments
-}
-
-type service struct {
-	repo         Repository
-	vesselClient vesselProto.VesselService
-}
-
-func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment, res *pb.Response) error {
-	logger.Infof("Received CreateConsignment request: %v", req)
-	vesselResponse, err := s.vesselClient.FindAvailable(context.Background(), &vesselProto.Specification{
-		MaxWeight: req.Weight,
-		Capacity:  int32(len(req.Containers)),
-	})
-	log.Printf("Found vessel: %s \n", vesselResponse.Vessel.Name)
-	if err != nil {
-		return err
-	}
-
-	req.VesselId = vesselResponse.Vessel.Id
-
-	consignment, err := s.repo.Create(req)
-	if err != nil {
-		return err
-	}
-
-	res.Created = true
-	res.Consignment = consignment
-	logger.Infof("Sent CreateConsignment response: %v", res)
-	return nil
-}
-
-func (s *service) GetConsignments(ctx context.Context, req *pb.GetRequest, res *pb.Response) error {
-	logger.Infof("Received GetConsignment request: %v", req)
-	consignments := s.repo.GetAll()
-	res.Consignments = consignments
-	logger.Infof("Sending GetConsignment response: %v", res)
-	return nil
-}
+const (
+	defaultHost = "datastore:27017"
+)
 
 func main() {
-	logger.DefaultLogger = logger.NewLogger(logger.WithLevel(logger.DebugLevel))
-	repo := &ConsignmentRepository{}
-
-	srv := micro.NewService(
-		micro.Name("go.micro.srv.consignment"),
-		micro.Version("latest"),
+	// Set-up micro instance
+	service := micro.NewService(
+		micro.Name("service.consignment"),
 	)
 
-	vesselClient := vesselProto.NewVesselService("go.micro.srv.vessel", srv.Client())
+	service.Init()
 
-	srv.Init()
+	uri := os.Getenv("DB_HOST")
+	if uri == "" {
+		uri = defaultHost
+	}
 
-	err := pb.RegisterShippingServiceHandler(srv.Server(), &service{repo, vesselClient})
+	client, err := CreateClient(context.Background(), uri, 0)
 	if err != nil {
-		log.Println(err)
+		log.Panic(err)
+	}
+	defer func(client *mongo.Client, ctx context.Context) {
+		err := client.Disconnect(ctx)
+		if err != nil {
+
+		}
+	}(client, context.Background())
+
+	consignmentCollection := client.Database("mymongo").Collection("consignments")
+
+	repository := &MongoRepository{consignmentCollection}
+	vesselClient := vesselProto.NewVesselService("service.client", service.Client())
+	h := &handler{repository, vesselClient}
+
+	registerErr := pb.RegisterShippingServiceHandler(service.Server(), h)
+	if registerErr != nil {
+		logger.Fatal(registerErr)
 		return
 	}
 
-	if err := srv.Run(); err != nil {
-		log.Println(err)
+	if err := service.Run(); err != nil {
+		logger.Error(err)
 	}
-	log.Println("Server Started!")
 }
